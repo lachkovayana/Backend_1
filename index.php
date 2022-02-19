59c1dfdf7889b6de79e2ffd5d466754d9e52c972
Yana Lachkova <lyana688@gmail.com>
2022-02-19 20:09:02 +0700

    routers

--- index.php
+++ index.php
@@ -1,14 +1,34 @@
 <?php
-    header('Content-type: application/json');
 
+    function getData($method){
+        $data = new stdClass();
+        if ($method != 'GET'){
+            $data->body = json_decode(file_get_contents('php://input'));
+        }
+        $data->parameters = [];
+        $dataGet = $_GET;
+        foreach ($dataGet as $key => $value) {
+            if ($key != "q"){
+                $data ->parameters[$key] = $value;
+            }
+        }
+        return $data;
+    }
+
+
+    function getMethod(){
+        return $_SERVER['REQUEST_METHOD'];
+    }
+
+    header('Content-type: application/json');
+    
     $link = mysqli_connect("127.0.0.1", "backend_demo_1", "password", "backend_demo_1");
-    if (!$link){
+    if (!$link) {
         echo "Ошибка: Невозможно установить соединение с MySQL" . PHP_EOL;
         echo "Код ошибки: ", mysqli_connect_errno() . PHP_EOL;
         echo "Текст ошибки: ", mysqli_connect_error() . PHP_EOL;
         exit;
     }
-
 
     echo "Соединение с MySQL установлено!" , PHP_EOL;
     echo "Информация о сервере: " , mysqli_get_host_info($link) . PHP_EOL;
@@ -16,12 +36,10 @@
     $message=[];
     $message["users"] = [];
     $res = $link->query("SELECT id, name, login FROM users ORDER BY id ASC");
-    if (!$res)
-    {
+    if (!$res) {
         echo "Не удалось выполнить запрос: (" . $mysqli->errno . ") " . $mysqli->errno . PHP_EOL;
     }
-    else
-    {
+    else {
         while ($row = $res->fetch_assoc())
         {
             $message["users"][] = [
@@ -31,6 +49,22 @@
             ];
 
         }
-        echo json_encode($message);
     }
+
+    $url = isset($_GET['q']) ? $_GET['q'] : '';
+    $url = rtrim($url, '/');
+    $urlList = explode('/', $url);
+
+    $router = $urlList[0];
+    $requestData = getData(getMethod());
+
+    if (file_exists(realpath(dirname(__FILE__)).'/routers/' . $router . 'php')){
+        include_once 'routers/' . $router . 'php';
+        route($method, $urlList, $requestData);
+    }
+    else {
+        echo "NOPE 404";
+    }
+
+    
 ?>
---
+++ routers/user.php
@@ -0,0 +1,6 @@
+<?php
+    function route($method, $urlList, $requestData){
+        
+    }
+
+?>
