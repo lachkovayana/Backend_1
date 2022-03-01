0999bf927294218bb793cb1b306424592e7c4077
Yana Lachkova <lyana688@gmail.com>
2022-03-01 16:49:30 +0700

    API add in url

--- index.php
+++ index.php
@@ -33,19 +33,24 @@
     $url = rtrim($url, '/');
     $urlList = explode('/', $url);
 
-    $router = $urlList[0];
-    $method = getMethod();
-    $requestData = getData($method);
-    $UploadsDir = "uploads";
+    if ($urlList[0] == 'API'){
+        $urlList = array_slice($urlList, 1);
+        $router = $urlList[0];
+        $method = getMethod();
+        $requestData = getData($method);
+        $UploadsDir = "uploads";
 
 
-    if (file_exists( realpath(dirname(__FILE__)) . '/routers/' . $router . '.php' )) {
-        include_once 'routers/' . $router . '.php';
-        route($method, $urlList, $requestData);
+        if (file_exists( realpath(dirname(__FILE__)) . '/routers/' . $router . '.php' )) {
+            include_once 'routers/' . $router . '.php';
+            route($method, $urlList, $requestData);
+        }
+        else 
+            setHTTPStatus("404", "No such path");
+        
     }
-    else {
+    else
         setHTTPStatus("404", "No such path");
-    }
 
     
 ?>
